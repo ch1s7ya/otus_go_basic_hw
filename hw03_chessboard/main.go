2f6cecf5a2554d2da24b2813ca42d7cc740b1522
Evgeny Chistyakov <chist.evg@gmail.com>
2023-08-28 23:23:43 +0200

    Draw a chess board

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	boardSize := 8
+	black := "#"
+	white := " "
+	var boardChess string
+
+	fmt.Printf("Enter size of board: ")
+	fmt.Scanln(&boardSize)
+
+	for i := 0; i < boardSize; i++ {
+		for j := 0; j < boardSize; j++ {
+			if (i+j)%2 == 0 {
+				boardChess += white
+			} else {
+				boardChess += black
+			}
+		}
+		boardChess += "\n"
+	}
+
+	fmt.Println(boardChess)
 }
