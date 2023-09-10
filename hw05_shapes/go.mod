c372648c9939345b55e653754abcee91aa7ef8c7
Evgeny Chistyakov <chist.evg@gmail.com>
2023-09-10 22:43:14 +0200

    Add basis

--- hw05_shapes/go.mod
+++ hw05_shapes/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw05_shapes
+module github.com/ch1s7ya/otus_go_basic_hw/hw05_shapes
 
 go 1.20
--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	area() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c *circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+type rectangle struct {
+	length, width float64
+}
+
+func (r *rectangle) area() float64 {
+	return r.length * r.width
+}
+
+type triangle struct {
+	length, base float64
+}
+
+func (t *triangle) area() float64 {
+	return t.base * t.length / 2
+}
+
+func calculateArea(s any) float64 {
+	shape := s.(Shape)
+
+	return shape.area()
+}
+
 func main() {
-	// Place your code here.
+	c := circle{radius: 2}
+	a := calculateArea(c)
+	fmt.Println(a)
 }
