b5ee7bf5eba3d74181b4680331774b215a2d392c
Evgeny Chistyakov <chist.evg@gmail.com>
2023-09-28 21:11:00 +0200

    Add error handling

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -33,26 +33,46 @@ func (t *triangle) area() float64 {
 	return t.base * t.length / 2
 }
 
-func calculateArea(s any) float64 {
+func calculateArea(s any) (float64, error) {
 	shape, ok := s.(Shape)
 
 	if !ok {
-		fmt.Println("Object isn't Shape")
+		return 0, fmt.Errorf("Object isn't Shape")
 	}
 
-	return shape.area()
+	return shape.area(), nil
 }
 
 func main() {
 	c := circle{radius: 5}
-	a := calculateArea(&c)
+	a, err := calculateArea(&c)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("Круг: радиус %.2f\nПлощадь: %.2f\n", c.radius, a)
 
 	r := rectangle{10, 5}
-	a = calculateArea(&r)
+	a, err = calculateArea(&r)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("Прямоугольник: высота %.2f, ширина: %.2f\nПлощадь: %.2f\n", r.length, r.width, a)
 
 	t := triangle{8, 6}
-	a = calculateArea(&t)
+	a, err = calculateArea(&t)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("Треугольник: высота %.2f, основание: %.2f\nПлощадь: %.2f\n", t.length, t.base, a)
+
+	someObject := struct {
+		a float64
+		b float64
+	}{
+		a: 2,
+		b: 5}
+	_, err = calculateArea(&someObject)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
