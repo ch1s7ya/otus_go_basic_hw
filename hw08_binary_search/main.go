d50fbbd922d41154da1e674cb701cf78144e6d8f
Evgeny Chistyakov <chist.evg@gmail.com>
2023-10-17 00:22:31 +0200

    Add BinarySearch function and tests

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+func BinarySearch(s []int, searchElement int) (bool, error) {
+	sort.Ints(s)
+	leftElement := 0
+	rightElement := len(s) - 1
+	if searchElement > s[rightElement] || searchElement < s[leftElement] {
+		return false, fmt.Errorf("search element is out of range")
+	}
+	for leftElement <= rightElement {
+		middleElement := (leftElement + rightElement) / 2
+		switch {
+		case searchElement < s[middleElement]:
+			rightElement = middleElement
+		case searchElement > s[middleElement]:
+			leftElement = middleElement
+		case searchElement == s[middleElement]:
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
 func main() {
-	// Place your code here.
+	fmt.Println(BinarySearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 6))
 }
---
+++ hw08_binary_search/main_test.go
@@ -0,0 +1,65 @@
+package main
+
+import "testing"
+
+func TestBinarySearch(t *testing.T) {
+	type args struct {
+		s             []int
+		searchElement int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "Sorted array from 0 to 9",
+			args: args{
+				[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+				7,
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "Unsorted array from 0 to 9",
+			args: args{
+				[]int{7, 1, 9, 5, 4, 3, 6, 0, 8, 2},
+				7,
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "Search element is out of range",
+			args: args{
+				[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+				42,
+			},
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name: "Boundary condition",
+			args: args{
+				[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+				0,
+			},
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := BinarySearch(tt.args.s, tt.args.searchElement)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BinarySearch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("BinarySearch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
